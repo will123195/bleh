8066ab9cfd087dfaecc7c6a8e9c47b64bce1ca17
will123195 <will123195@gmail.com>
2015-08-29 13:48:48 -0400

    helpers

--- sample-app/index.js
+++ sample-app/index.js
@@ -1,7 +1,7 @@
 var bleh = require('..')
 var app = bleh({
   main: __dirname, // needed for tests
-  handlebarsHelpers: 'lib/handlebars-helpers.js',
+  //handlebarsHelpers: 'lib/handlebars-helpers.js',
   sessions: {
     secret: 'some unique random key with plenty of entropy'
   }
